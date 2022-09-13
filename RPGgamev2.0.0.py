3f03f1c8b74f26a47f8259b2db6fab86ff945e8d
Bl0f3lt <93886321+Bl0f3lt@users.noreply.github.com>
2022-09-13 22:32:07 +0100

    First Commit
    The first iteration of the JSON loot table, and beginning of the rewrite of the game from flawed v1.0.0 to v2.0.0

---
+++ RPGgamev2.0.0.py
@@ -0,0 +1,8 @@
+#RPG Game v2.0.0
+#The rewrite
+
+#The same original text dungeon game but rewritten to be easier to expand, more efficent, and most importantly more fun
+
+#importing libs
+import random
+import json
